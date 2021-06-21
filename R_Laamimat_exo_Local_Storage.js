0e9912011a0ded85ae67f4ca2275e9f68f3d2502
mattCornicDev <glgharib@gmail.com>
2021-06-21 13:46:04 +0200

    first commit

---
+++ R_Laamimat_exo_Local_Storage.js
@@ -0,0 +1,9 @@
+// 
+
+function saisirDesContacts() {
+    window.location.href="saisirDesContacts.html";
+  }
+  function voirLesCourbesDesPatients() {
+    window.location.href="voirLesContactsSauvegardes.html";
+  }
+  
